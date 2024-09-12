df0e3f443403d7404ef585d31180bd10bed123e1
upasana_anand <upasana_anand@epam.com>
2024-09-12 11:27:26 +0530

    maven-serenity-cucumber-project - Task 04_Hometask_Maven

---
+++ serenity-regression/src/test/java/RegressionTest.java
@@ -0,0 +1,11 @@
+
+import org.testng.annotations.Test;
+
+
+public class RegressionTest {
+
+    @Test
+    public void display() {
+        System.out.println("regression test");
+    }
+}
---
+++ serenity-smoke/src/test/java/SmokeTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SmokeTest {
+
+    @Test
+    public void display() {
+        System.out.println("smoke test");
+    }
+}
